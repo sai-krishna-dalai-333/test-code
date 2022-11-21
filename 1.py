3f26d1e906e8d146807f60434eb405becc98fb8a
sai-krishna-dalai-333 <85897763+sai-krishna-dalai-333@users.noreply.github.com>
2022-11-21 16:33:32 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,14 +1,10 @@
 def base3(n):
-    if(n==0):
-        return
-    x=n%3
-    n//=3
-    if(x<0):
-        n+=1
-    base3(n)
-    if(x<0):
-        print(x+(3*-1),end="")
-    else:
-        print(x,end="")
+    s = ""
+    while (n > 0):
+        r = n % 3
+        s += str(r)
+        n = n // 3
+    s = s[::-1]
+    return s
 n=int(input())
 print(base3(n))
